982fb302e41746115f5261732ed1c1b2b821549b
coffee-is-power <tiagodinis33@proton.me>
2023-10-03 11:59:46 +0100

    feat. add ficha3 (ex2)

--- ficha3.c
+++ ficha3.c
@@ -12,5 +12,14 @@ int main() {
   printf("Girls (N/%%)\t| Boys (N/%%)\n");
   printf("-----------------------------------\n");
   printf("%d (%.2f%%)\t| %d (%.2f%%)\n", girls, girls_percentage*100, boys, boys_percentage*100);
+  
+  printf("\nExercicio 2\n");
+  printf("--------------------------------\n");
+
+  int A = 23;
+  int B = 47;
+  int C = 30;
+  float media = ((float) (A + B + C)) / 3.0;
+  printf("media=%f\nA-media=%f\nB-media=%f\nC-media=%f\n", media, A-media, B-media, C-media);
   return 0;
 }
