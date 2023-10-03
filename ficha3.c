46bd765bd2a5890518671346ad232d6f85637a03
coffee-is-power <tiagodinis33@proton.me>
2023-10-03 11:59:30 +0100

    feat: add ficha3 (ex1)

--- ficha3.c
+++ ficha3.c
@@ -1,3 +1,16 @@
-//
-// Created by tiago on 29-09-2023.
-//
+#include <stdio.h>
+
+int main() {
+  printf("Exercicio 1\n");
+  printf("--------------------------------\n");
+
+  int boys = 26;
+  int girls = 4;
+  int total = girls + boys;
+  double girls_percentage = ((double) girls) / total;
+  double boys_percentage = ((double) boys) / total;
+  printf("Girls (N/%%)\t| Boys (N/%%)\n");
+  printf("-----------------------------------\n");
+  printf("%d (%.2f%%)\t| %d (%.2f%%)\n", girls, girls_percentage*100, boys, boys_percentage*100);
+  return 0;
+}
